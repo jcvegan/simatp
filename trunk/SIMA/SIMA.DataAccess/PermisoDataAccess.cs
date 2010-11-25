0703ddfb5777dd0fda93030bb3fefce89280a52e
nevecaju@gmail.com <nevecaju@gmail.com@5a415786-5b86-9bb9-b26c-d4fdf2eeb2ab>
2010-11-25 02:56:19 +0000

    Toda la seguridad implementada

---
+++ trunk/SIMA/SIMA.DataAccess/PermisoDataAccess.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SIMA.DataAccess.Base;
+using SIMA.Entities;
+
+namespace SIMA.DataAccess
+{
+    public class PermisoDataAccess:DataAccessBase
+    {
+        public T_C_Permiso SeleccionarPermiso(int idPermiso)
+        {
+            try
+            {
+                T_C_Permiso permiso = new T_C_Permiso();
+                Connection = new System.Data.SqlClient.SqlConnection(ConnectionString);
+                using(Command = new System.Data.SqlClient.SqlCommand("T_C_PermisoSelect",Connection))
+                {
+                    Command.CommandType = System.Data.CommandType.StoredProcedure;
+                    Command.Parameters.AddWithValue("@Id_Permiso", idPermiso);
+                    Connection.Open();
+                    System.Data.SqlClient.SqlDataReader reader = Command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        permiso.Id_Permiso = Convert.ToInt32(reader.GetValue(reader.GetOrdinal("Id_Permiso")));
+                        permiso.Nombre = reader.GetValue(reader.GetOrdinal("Nombre")).ToString();
+                        permiso.Descripcion = reader.GetValue(reader.GetOrdinal("Descripcion")).ToString();
+                        permiso.Id_Estado = Convert.ToInt32(reader.GetValue(reader.GetOrdinal("Id_Estado")));
+                        permiso.Estado = new EstadoDataAccess().Seleccionar(permiso.Id_Estado);
+                    }
+                }
+                return permiso;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+        }
+    }
+}
